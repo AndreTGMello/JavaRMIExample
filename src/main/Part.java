4bdde8cb06934e2b560dc3c4d6e959ebfd506c41
André Thomaz Gandolpho de Mello <andre.tgmello@gmail.com>
2016-04-04 14:41:45 -0300

    Criacao dos metodos das interfaces.

--- src/main/Part.java
+++ src/main/Part.java
@@ -1,5 +1,18 @@
 package main;
 
-public class Part {
+import java.util.ArrayList;
 
+public interface Part {
+	public static int getCod(){
+		return 0;
+	}
+	public static String getNome(){
+		return "";
+	}
+	public static String getDesc(){
+		return "";
+	}
+	public static ArrayList<Part> getSubComp(){
+		return null;
+	}
 }
--- src/main/PartRepository.java
+++ src/main/PartRepository.java
@@ -1,5 +1,14 @@
 package main;
 
-public class PartRepository {
+import java.rmi.Remote; 
+import java.rmi.RemoteException; 
 
+public interface PartRepository extends Remote{
+	public static void getListaPart(){	
+	}
+	public static Part getPart(int cod){
+		return null;	
+	}
+	public static void addPart(Part p){	
+	}
 }
