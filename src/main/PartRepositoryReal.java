dec57df53d2176d756580a2023f030d8a4e616c4
André Thomaz Gandolpho de Mello <andre.tgmello@gmail.com>
2016-04-04 15:21:59 -0300

    Implementacao da interface PartRepository.

---
+++ src/main/PartRepositoryReal.java
@@ -0,0 +1,34 @@
+package main;
+
+import javax.rmi.PortableRemoteObject;
+import java.rmi.*;
+import javax.swing.*;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+
+public class PartRepositoryReal extends UnicastRemoteObject implements PartRepository {
+
+	ArrayList<Part> partsArmazenadas;
+	String nomeArmazem;
+	
+	public PartRepositoryReal(String nomeArmazem) throws RemoteException{
+		this.partsArmazenadas = new ArrayList<>();
+		this.nomeArmazem = nomeArmazem;
+	}
+
+	@Override
+	public ArrayList<Part> getListaPart() {
+		return partsArmazenadas;
+	}
+
+	@Override
+	public Part getPart(int cod) {
+		return partsArmazenadas.get(cod);
+	}
+
+	@Override
+	public void addPart(Part p) {
+		partsArmazenadas.add(p);
+	}
+
+}
