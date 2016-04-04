852bbdcf7f51042fc03bbd7b36e23dc51914c9b8
André Thomaz Gandolpho de Mello <andre.tgmello@gmail.com>
2016-04-04 16:04:09 -0300

    Implementacao dos comandos de manipulacao da classe UsuarioCliente.

--- src/main/UsuarioClient.java
+++ src/main/UsuarioClient.java
@@ -2,6 +2,7 @@ package main;
 
 import java.io.File;
 import java.rmi.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import javax.swing.*;
@@ -10,14 +11,70 @@ public class UsuarioClient {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String repositorioCorrente;
-		Part pecaCorrente;
+		String nomeArmazem;
+		PartRepository repositorioCorrente = null;
+		Part pecaCorrente = null;
+		ArrayList<Part> subPecasCorrente = null;
 		
-		while(scan.hasNext()){
+		
+		while(!scan.next().equals("quit")){
 			String comando = scan.next();
 			if(comando.equals("bind")){
 				System.out.println("Insira o nome do armazem a ser acessado:");
-				String nomeArmazem = scan.next();
+				nomeArmazem = scan.next();
+				
+				try{
+					repositorioCorrente = (PartRepository) Naming.lookup(nomeArmazem);
+				}catch(RemoteException re){
+		            System.out.println("Erro Remoto: "+re.toString());
+		        }
+		        catch(Exception e){
+		            System.out.println("Erro Local: "+e.toString());
+		        }
+			}
+			else if(comando.equals("listp")){
+				ArrayList<Part> listaPart = repositorioCorrente.getListaPart();
+				for (Part part : listaPart) {
+					System.out.print(part+" | ");
+				}
+				System.out.println();
+			}
+			else if(comando.equals("getp")){
+				System.out.println("Insira o codigo da peca a ser buscada:");
+				int cod = scan.nextInt();
+				if(repositorioCorrente.getPart(cod)!=null){
+					pecaCorrente = repositorioCorrente.getPart(cod);
+				}
+				else System.out.println("Peca nao encontrada, tente novamente.");
+			}
+			else if(comando.equals("showp")){
+				System.out.println("Codigo da peca: "
+						+ pecaCorrente.getCod()
+						+ "\nNome da peca: "
+						+ pecaCorrente.getNome()
+						+ "\nDescricao da peca: "
+						+ pecaCorrente.getDesc()
+						+ "\nLocal de armazenamento: "
+						+ pecaCorrente.getLocalArmazenado()
+						+ "Tipo: "
+						+ pecaCorrente.primitivaOuAgregada()
+						+ "Quantidade de subcomponentes: "
+						+ pecaCorrente.getQtdSubComp());
+				if(pecaCorrente.getQtdSubComp()>0){
+					System.out.println("Subcomponentes: ");
+					for (Part part : pecaCorrente.getSubComp()) {
+						System.out.print(part+" | ");
+					}
+					System.out.println();
+				}
+			}
+			else if(comando.equals("clearlist")){
+				subPecasCorrente = new ArrayList<>();
+			}
+			else if(comando.equals("addsubpart")){
+				subPecasCorrente = new ArrayList<>();
+			}
+			else if(comando.equals("addp")){
 				
 			}
 		}
