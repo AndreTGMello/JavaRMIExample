46d2d1ebc65e0e585ee4a60b1176927503febdfe
André Thomaz Gandolpho de Mello <andre.tgmello@gmail.com>
2016-04-04 15:04:51 -0300

    Correcao dos metodos das classes interface.

--- src/main/Part.java
+++ src/main/Part.java
@@ -3,25 +3,11 @@ package main;
 import java.util.ArrayList;
 
 public interface Part {
-	public static int getCod(){
-		return 0;
-	}
-	public static String getNome(){
-		return "";
-	}
-	public static String getDesc(){
-		return "";
-	}
-	public static String getLocalArmazenado(){
-		return "";
-	}
-	public static ArrayList<Part> getSubComp(){
-		return null;
-	}
-	public static int getQtdSubComp(){
-		return 0;
-	}
-	public static String primitivaOuAgregada(){
-		return "";
-	}
+	int getCod();
+	String getNome();
+	String getDesc();
+	String getLocalArmazenado();
+	ArrayList<Part> getSubComp();
+	int getQtdSubComp();
+	String primitivaOuAgregada();
 }
--- src/main/PartRepository.java
+++ src/main/PartRepository.java
@@ -4,11 +4,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException; 
 
 public interface PartRepository extends Remote{
-	public static void getListaPart(){	
+	void getListaPart();
+	Part getPart(int cod);
+	void addPart(Part p);
 	}
-	public static Part getPart(int cod){
-		return null;	
-	}
-	public static void addPart(Part p){	
-	}
-}
