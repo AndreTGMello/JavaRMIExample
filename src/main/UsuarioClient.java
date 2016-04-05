2725928074ddf6e2535c32d3634015da6911ba30
André Thomaz Gandolpho de Mello <andre.tgmello@gmail.com>
2016-04-04 21:40:08 -0300

    Todos os metodos sugeridos na documentacao implementados.

--- src/main/UsuarioClient.java
+++ src/main/UsuarioClient.java
@@ -3,7 +3,13 @@ package main;
 import java.io.File;
 import java.rmi.*;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.Map.Entry;
+import java.util.Objects;
 
 import javax.swing.*;
 
@@ -11,10 +17,10 @@ public class UsuarioClient {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String nomeArmazem;
+		String nomeArmazem = null;
 		PartRepository repositorioCorrente = null;
 		Part pecaCorrente = null;
-		ArrayList<Part> subPecasCorrente = null;
+		Map<Part, Integer> subPecasCorrente = new HashMap<>();
 		
 		
 		while(!scan.next().equals("quit")){
@@ -31,23 +37,29 @@ public class UsuarioClient {
 		        catch(Exception e){
 		            System.out.println("Erro Local: "+e.toString());
 		        }
+				
 			}
 			else if(comando.equals("listp")){
+				
 				ArrayList<Part> listaPart = repositorioCorrente.getListaPart();
 				for (Part part : listaPart) {
 					System.out.print(part+" | ");
 				}
 				System.out.println();
+				
 			}
 			else if(comando.equals("getp")){
+				
 				System.out.println("Insira o codigo da peca a ser buscada:");
 				int cod = scan.nextInt();
 				if(repositorioCorrente.getPart(cod)!=null){
 					pecaCorrente = repositorioCorrente.getPart(cod);
 				}
 				else System.out.println("Peca nao encontrada, tente novamente.");
+				
 			}
 			else if(comando.equals("showp")){
+				
 				System.out.println("Codigo da peca: "
 						+ pecaCorrente.getCod()
 						+ "\nNome da peca: "
@@ -62,25 +74,59 @@ public class UsuarioClient {
 						+ pecaCorrente.getQtdSubComp());
 				if(pecaCorrente.getQtdSubComp()>0){
 					System.out.println("Subcomponentes: ");
-					for (Part part : pecaCorrente.getSubComp()) {
-						System.out.print(part+" | ");
+					Map<Part, Integer> subComp = pecaCorrente.getSubComp();
+					for (Map.Entry<Part, Integer> entry : subComp.entrySet())
+					{
+					    System.out.println("Subcomponente" + entry.getValue() 
+					    + ". Quantidade: " + entry.getValue());
 					}
-					System.out.println();
 				}
+				
 			}
 			else if(comando.equals("clearlist")){
-				subPecasCorrente = new ArrayList<>();
+				
+				subPecasCorrente = new HashMap<>();
+
 			}
 			else if(comando.equals("addsubpart")){
+			
 				System.out.println("Numero de pecas que deseja adicionar: ");
 				int qtd = scan.nextInt();
-				subPecasCorrente.add(pecaCorrente);
+				if(subPecasCorrente.containsValue(pecaCorrente)){
+					subPecasCorrente.replace(pecaCorrente, 
+							subPecasCorrente.get(pecaCorrente), 
+							subPecasCorrente.get(pecaCorrente)+qtd);
+				}
+				else{
+					subPecasCorrente.put(pecaCorrente, qtd);
+				}
+				
 			}
 			else if(comando.equals("addp")){
-				
+				Part p = criaPeca(subPecasCorrente, nomeArmazem);
+				repositorioCorrente.addPart(p);
 			}
 		}
 
+	}
+
+	private static Part criaPeca(Map<Part, Integer> subComponentes, String localArmazenado) {
+		int cod = 0;
+		String nome = "";
+		String desc = "";
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite o codigo da peca: ");
+		cod = scan.nextInt();
+		
+		System.out.println("Digite o nome da peca: ");
+		nome = scan.next();
+		
+		System.out.println("Digite a descricao da peca: ");
+		desc = scan.nextLine();
+		
+		PartReal p = new PartReal(cod, nome, desc, localArmazenado, subComponentes);
+		return p;
 	}
 
 }
